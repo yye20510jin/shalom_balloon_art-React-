3356793ad3bf5214245ff2add9f53f5620163d0b
yye20510jin <yye20510@naver.com>
2025-11-29 12:52:56 +0900

    fix: rename file casing

--- src/router/routes.jsx
+++ src/router/routes.jsx
@@ -2,11 +2,12 @@ import Home from "../Home";
 import AdminLogin from "../admin/AdminLogin";
 import Admin from "../admin/Admin"
 import Membership from "../Membership";
-
+import UserLogin from "../User/UserLogin";
 
 export const routes=[
     {path: "/", element: <Home />},
     {path:"/adminLogin", element: <AdminLogin />},
     {path:"/admin", element: <Admin/>},
-    {path:"/membership", element:<Membership/>}
+    {path:"/membership", element:<Membership/>},
+    {path:"/userLogin",element:<UserLogin/>}
 ];
